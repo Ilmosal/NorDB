bbc90cee98c38490d2cc6d8c13c409cb370a24e8
MrCubanfrog <ilmosal@gmail.com>
2017-09-04 12:42:44 +0300

    Added database initialization and destruction.

--- nordb/database/initNorDB.py
+++ nordb/database/initNorDB.py
@@ -1,31 +1,94 @@
 import sys
-import logger
-import logger
+import logging
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+username = "ilmosalm"
 
 #TODO: init_db
 def init_database():
-	pass
+	conn = psycopg2.connect("dbname=postgres user={0}".format(username))
+	conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+	cur = conn.cursor()
+
+	cur.execute("SELECT 1 FROM pg_database WHERE datname='nordb'")
+	if cur.fetchall():
+		logging.error("Database already exists. Destroy the database with -d and try again!")
+		conn.close()
+		sys.exit()
+
+	cur.execute(open("../nordsql/init_nordb.sql", "r").read())
+
+	conn.commit()
+	conn.close()
+
+	conn = psycopg2.connect("dbname=nordb user={0}".format(username))
+	cur = conn.cursor()
+	
+	cur.execute(open("../nordsql/nordic_event_root.sql", "r").read())
+	cur.execute(open("../nordsql/nordic_file.sql", "r").read())
+	cur.execute(open("../nordsql/nordic_event.sql", "r").read())
+	cur.execute(open("../nordsql/scandia_header.sql", "r").read())
+	cur.execute(open("../nordsql/nordic_modified.sql", "r").read())
+	cur.execute(open("../nordsql/nordic_header_main.sql", "r").read())
+	cur.execute(open("../nordsql/nordic_header_error.sql", "r").read())
+	cur.execute(open("../nordsql/nordic_header_macroseismic.sql", "r").read())
+	cur.execute(open("../nordsql/nordic_header_waveform.sql", "r").read())
+	cur.execute(open("../nordsql/nordic_phase_data.sql", "r").read())
+
+	conn.commit()
+	conn.close()
 
 #TODO: destroy db
 def destroy_database():
+	conn = psycopg2.connect("dbname=postgres user={0}".format(username))
+	conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+	cur = conn.cursor()
+
+	cur.execute("SELECT 1 FROM pg_database WHERE datname='nordb'")
+	if not cur.fetchall():
+		logging.error("Database doesn't exists. Exiting program.")
+		conn.close()
+		sys.exit()
+
+	conn.close()
+
+	conn = psycopg2.connect("dbname=nordb user={0}".format(username))
+	cur = conn.cursor()
+
+	cur.execute("DROP SCHEMA public CASCADE")
+
+	conn.commit()
+	conn.close()
+
+
+	conn = psycopg2.connect("dbname=postgres user={0}".format(username))
+	conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+	cur = conn.cursor()
+
+	cur.execute("DROP DATABASE nordb")
+
+	conn.commit()
+	conn.close()
+
 	pass
 
 #TODO: help function
 def print_help():
 	pass
 
-if __name__="__main__":
+if __name__ == "__main__":
 	if len(sys.argv) > 1:
 		if sys.argv[1] == "-d":
-			logger.info("Destroying the database")
+			logging.info("Destroying the database")
 			destroy_database()
-			logger.info("Database has been destroyed")
-		elif sys.argv[1] == "-i"
-			logger.info("Initializing the database")
+			logging.info("Database has been destroyed")
+		elif sys.argv[1] == "-i":
+			logging.info("Initializing the database")
 			init_database()
-			logger.info("Database has been Initialized")
+			logging.info("Database has been Initialized")
 		else:
-			logger.error("No such flag. Type -h for help.")
+			logging.error("No such flag. Type -h for help.")
 			sys.exit()
 	else:
-		logger.error("No flag given for the program. Type -h for help")	
+		logging.error("No flag given for the program. Type -h for help")	
