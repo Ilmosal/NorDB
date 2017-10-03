77b2a259c076f3445b9b96346af7b7ddf5681d88
MrCubanfrog <ilmosal@gmail.com>
2017-10-03 18:27:21 +0300

    small changes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
 	name="NorDB",
-	version="0.1.0",
+	version="0.1.1",
 	author="Ilmo Salmenperä",
 	author_email="ilmo.salmenpera@helsinki.fi",
 	packages=["nordb", "nordb/database", "nordb/io", "nordb/validation", "nordb/sql"],
@@ -10,5 +10,12 @@ setup(
 	url="http://github.com/MrCubanfrog/NorDB",
 	licence="LICENSE",
 	description="Library for handling a seismic event database based on the nordic format",
+	install_requires=[
+		"psycopg2",
+	],
 	long_description=open("README.md").read(),
+	entry_points='''
+		[console_scripts]
+		nordb=bin.run.py:nordb
+	''',
 )
