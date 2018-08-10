f5a52e6049bad58b74be385f07614f33b6f60c9b
MrCubanfrog <ilmosal@gmail.com>
2018-08-10 10:48:52 +0300

    Fixed a small bug with observation time of the NordicData object

--- nordb/nordic/nordicData.py
+++ nordb/nordic/nordicData.py
@@ -14,7 +14,7 @@ from nordb.core.utils import addFloat2String
 
 class NordicData:
     """
-    A class that functions as a collection of enums. Contains the information of the phase data line of a nordic file. 
+    A class that functions as a collection of enums. Contains the information of the phase data line of a nordic file.
 
     :param list data: The data of a nordic phase in a list where each index of a value corresponds to NordicData's pseudo-enum. The data easily accessed with the enums provided by the class
     :ivar string station_code: Code of the station. Maximum of 4 characters
@@ -23,7 +23,7 @@ class NordicData:
     :ivar string quality_indicator: Quality of the observation(I, E, etc.)
     :ivar string phase_type: Phase type. (PG, SG, PN, etc). Maximum of 4 characters
     :ivar int weight: Weighting indicator(1-4). 0=full weight as Hypocenter program, 9=timing error
-    :ivar string first_motion: First motion of the observation(C/D for clear compression/dilatation, +/- for unclear compression/dilatation). 
+    :ivar string first_motion: First motion of the observation(C/D for clear compression/dilatation, +/- for unclear compression/dilatation).
     :ivar observation_time datetime: Datetime of the pick
     :ivar int signal_duration:  duration of the signal from phase onset to noise as seconds
     :ivar float max_amplitude: Maximum amplitude from zero to peak ground motion amplitude.
@@ -42,7 +42,7 @@ class NordicData:
     :ivar int STATION_CODE: Location of the station_code in a array. Value of 0
     :ivar int SP_INSTRUMENT_TYPE: Location of the sp_instrument_type in a array. Value of 1
     :ivar int SP_COMPONENT: Location of the sp_component in a array. Value of 2
-    :ivar int QUALITY_INDICATOR: Location of the quality_indicator in a array. Value of 3 
+    :ivar int QUALITY_INDICATOR: Location of the quality_indicator in a array. Value of 3
     :ivar int PHASE_TYPE: Location of the phase_type in a array. Value of 4
     :ivar int WEIGHT: Location of the weight in a array. Value of 5
     :ivar int FIRST_MOTION: Location of the first_motion in a array. Value of 6
@@ -65,7 +65,7 @@ class NordicData:
     STATION_CODE = 0
     SP_INSTRUMENT_TYPE = 1
     SP_COMPONENT = 2
-    QUALITY_INDICATOR = 3 
+    QUALITY_INDICATOR = 3
     PHASE_TYPE = 4
     WEIGHT = 5
     FIRST_MOTION = 6
@@ -145,119 +145,119 @@ class NordicData:
         self._sp_instrument_type = val_sp_instrument_type
 
     sp_component = property(operator.attrgetter('_sp_component'), doc="")
-    
+
     @sp_component.setter
     def sp_component(self, val):
         val_sp_component = validateString(val, "sp_component", 0, 1, "ZNEH12VRT", self.header_type)
         self._sp_component = val_sp_component
 
     quality_indicator = property(operator.attrgetter('_quality_indicator'), doc="")
-    
+
     @quality_indicator.setter
     def quality_indicator(self, val):
         val_quality_indicator = validateString(val, "quality_indicator", 0, 1, None, self.header_type)
         self._quality_indicator = val_quality_indicator
 
     phase_type = property(operator.attrgetter('_phase_type'), doc="")
-    
+
     @phase_type.setter
     def phase_type(self, val):
         val_phase_type = validateString(val, "phase_type", 0, 4, None, self.header_type)
         self._phase_type = val_phase_type
 
     weight = property(operator.attrgetter('_weight'), doc="")
-    
+
     @weight.setter
     def weight(self, val):
         val_weight = validateInteger(val, "weight", 0, 9, self.header_type)
         self._weight = val_weight
 
     first_motion = property(operator.attrgetter('_first_motion'), doc="")
-    
+
     @first_motion.setter
     def first_motion(self, val):
         val_first_motion = validateString(val, "first_motion", 0, 1, "+-DC", self.header_type)
         self._first_motion = val_first_motion
 
     observation_time = property(operator.attrgetter('_observation_time'), doc="")
-    
+
     @observation_time.setter
     def observation_time(self, val):
         val_observation_time = validateDatetime(val, "observation_time", self.header_type)
         self._observation_time = val_observation_time
 
     signal_duration = property(operator.attrgetter('_signal_duration'), doc="")
-    
+
     @signal_duration.setter
     def signal_duration(self, val):
         val_signal_duration = validateInteger(val, "signal_duration", 0, 9999, self.header_type)
         self._signal_duration = val_signal_duration
 
     max_amplitude = property(operator.attrgetter('_max_amplitude'), doc="")
-    
+
     @max_amplitude.setter
     def max_amplitude(self, val):
         val_max_amplitude = validateFloat(val, "max_amplitude", -1.0, 9999.9, self.header_type)
         self._max_amplitude = val_max_amplitude
 
     max_amplitude_period = property(operator.attrgetter('_max_amplitude_period'), doc="")
-    
+
     @max_amplitude_period.setter
     def max_amplitude_period(self, val):
         val_max_amplitude_period = validateFloat(val, "max_amplitude_period", -1.0, 99.9, self.header_type)
         self._max_amplitude_period = val_max_amplitude_period
 
     back_azimuth = property(operator.attrgetter('_back_azimuth'), doc="")
-    
+
     @back_azimuth.setter
     def back_azimuth(self, val):
         val_back_azimuth = validateFloat(val, "back_azimuth", 0.0, 359.9, self.header_type)
         self._back_azimuth = val_back_azimuth
 
     apparent_velocity = property(operator.attrgetter('_apparent_velocity'), doc="")
-    
+
     @apparent_velocity.setter
     def apparent_velocity(self, val):
         val_apparent_velocity = validateFloat(val, "apparent_velocity", 0.0, 99.9, self.header_type)
         self._apparent_velocity = val_apparent_velocity
 
     signal_to_noise = property(operator.attrgetter('_signal_to_noise'), doc="")
-    
+
     @signal_to_noise.setter
     def signal_to_noise(self, val):
         val_signal_to_noise = validateFloat(val, "signal_to_noise", 0.0, 99.9, self.header_type)
         self._signal_to_noise = val_signal_to_noise
 
     azimuth_residual = property(operator.attrgetter('_azimuth_residual'), doc="")
-    
+
     @azimuth_residual.setter
     def azimuth_residual(self, val):
         val_azimuth_residual = validateInteger(val, "azimuth_residual", -99, 999, self.header_type)
         self._azimuth_residual = val_azimuth_residual
 
     travel_time_residual = property(operator.attrgetter('_travel_time_residual'), doc="")
-    
+
     @travel_time_residual.setter
     def travel_time_residual(self, val):
         val_travel_time_residual = validateFloat(val, "travel_time_residual", -999.9, 9999.9, self.header_type)
         self._travel_time_residual = val_travel_time_residual
 
     location_weight = property(operator.attrgetter('_location_weight'), doc="")
-    
+
     @location_weight.setter
     def location_weight(self, val):
         val_location_weight = validateInteger(val, "location_weight", 0, 10, self.header_type)
         self._location_weight = val_location_weight
 
     epicenter_distance = property(operator.attrgetter('_epicenter_distance'), doc="")
-    
+
     @epicenter_distance.setter
     def epicenter_distance(self, val):
         val_epicenter_distance = validateInteger(val, "epicenter_distance", 0, 99999, self.header_type)
         self._epicenter_distance = val_epicenter_distance
 
     epicenter_to_station_azimuth = property(operator.attrgetter('_epicenter_to_station_azimuth'), doc="")
-    
+
     @epicenter_to_station_azimuth.setter
     def epicenter_to_station_azimuth(self, val):
         val_epicenter_to_station_azimuth = validateInteger(val, "epicenter_to_station_azimuth", 0, 359, self.header_type)
@@ -267,21 +267,20 @@ class NordicData:
         phase_string = " "
         phase_string += addString2String(self.station_code, 4, '<')
         phase_string += " "
-        phase_string += addString2String(self.sp_instrument_type, 1, '<') 
+        phase_string += addString2String(self.sp_instrument_type, 1, '<')
         phase_string += addString2String(self.sp_component, 1, '<')
         phase_string += " "
-        phase_string += addString2String(self.quality_indicator, 1, '<')  
+        phase_string += addString2String(self.quality_indicator, 1, '<')
         phase_string += addString2String(self.phase_type, 4, '<')
         phase_string += addInteger2String(self.weight, 1, '<')
         phase_string += " "
         phase_string += addString2String(self.first_motion, 1, '<')
-        phase_string += " " #HOW TO IMPLEMENT TIME INFO WITHOUT THE KNOWLEDGE OF THE REAL DAY?!
+        phase_string += " "
         phase_string += addInteger2String(self.observation_time.hour, 2, '0')
         phase_string += addInteger2String(self.observation_time.minute, 2, '0')
         phase_string += " "
-        phase_string += addInteger2String(self.observation_time.second, 2, '0')
-        phase_string += "."
-        phase_string += addInteger2String(round(self.observation_time.microsecond/10000), 2, '0')
+        second = float(self.observation_time.second) + float(self.observation_time.microsecond)/1000000
+        phase_string += addFloat2String(second, 5, 2, '0')
         phase_string += " "
         phase_string += addInteger2String(self.signal_duration, 4, '>')
         phase_string += " "
@@ -295,7 +294,7 @@ class NordicData:
         phase_string += addFloat2String(self.signal_to_noise, 4, 1, '>')
         phase_string += addInteger2String(self.azimuth_residual, 3, '>')
         phase_string += addFloat2String(self.travel_time_residual, 5, 1, '>')
-        phase_string += addInteger2String(self.location_weight, 2, '>')   
+        phase_string += addInteger2String(self.location_weight, 2, '>')
         phase_string += addInteger2String(self.epicenter_distance, 5, '>')
         phase_string += " "
         phase_string += addInteger2String(self.epicenter_to_station_azimuth, 3, '>')
@@ -326,5 +325,5 @@ class NordicData:
         data_list.append(self.epicenter_to_station_azimuth)
         data_list.append(self.event_id)
 
-        return data_list 
+        return data_list
 
